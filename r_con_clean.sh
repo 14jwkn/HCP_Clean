5cc9448fe9ce9b95272cbcc3dece59549fb2ce1e
Kevin J Witczak <kevin.witczak@camh.ca>
2021-10-26 15:55:09 -0400

    fixup! fixup! fixup! Commit working example for discussion.

--- r_con_clean.sh
+++ r_con_clean.sh
@@ -25,8 +25,8 @@ module load gnu-parallel/20191122
 #module load singularity/3
 
 parallel --nn -j80 --keep-order \
-         'indir=${basedir}/{1}
-          outdir=${cleandir}/{1}
+         'indir='${basedir}'/{1}
+          outdir='${cleandir}'/{1}
           mkdir -p ${indir} ${outdir}
           ${prefix}/code/merge_HCPtxt_fixed.R '${outdir} ${indir}/'{2}' \
          :::: ${prefix}/code/r_test_sub.txt \
